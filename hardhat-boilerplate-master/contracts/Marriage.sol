04fca0be164f3c57b44552c24a37d9fba8082646
ToadyGamer <castellierflorian@gmail.com>
2024-04-24 17:25:21 +0200

    [UPDATE] - Mariage.sol to add function and data

--- hardhat-boilerplate-master/contracts/Marriage.sol
+++ hardhat-boilerplate-master/contracts/Marriage.sol
@@ -1,79 +1,54 @@
 //SPDX-License-Identifier: UNLICENSED
 
-// Solidity files have to start with this pragma.
-// It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
-// This is the main building block for smart contracts.
 contract Marriage {
-    // Some string type variables to identify the token.
-    string public name = "My Hardhat Marriage Token";
-    string public symbol = "MHMT";
+    string public name = "Contrat de Mariage";
+    string public symbol = "MARR";
 
-    // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    uint256 public totalSupply = 1; // Un seul contrat de mariage est créé
 
-    // An address type variable is used to store ethereum accounts.
     address public owner;
 
-    // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+    mapping(address => uint256) public balances; //Argent du mariage
 
-    // The Transfer event helps off-chain aplications understand
-    // what happens within your contract.
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    /**
-     * Contract initialization.
-     */
-    constructor() {
-        // The totalSupply is assigned to the transaction sender, which is the
-        // account that is deploying the contract.
+    // Informations sur le mariage
+    string public weddingDate;
+    Child[] public children;
+
+    string public nameMaryMan;
+    string public nameMaryWomen;
+
+    //Object enfant avec un nom prenim et age
+    struct Child {
+        string firstName;
+        string lastName;
+        uint age;
+    }
+
+    //On met en place les données du mariage
+    constructor(string memory _weddingDate, string memory _maryMan, string memory _maryWomen) {
         balances[msg.sender] = totalSupply;
         owner = msg.sender;
+
+        weddingDate = _weddingDate;
+        nameMaryMan = _maryMan;
+        nameMaryWomen = _maryWomen;
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-
-        // Notify off-chain applications of the transfer.
-        emit Transfer(msg.sender, to, amount);
+    //On ajoute un petit enfant trop pipou
+    function addChild(string memory _firstName, string memory _lastName, uint _age) public {
+        require(msg.sender == owner, "Only the owner can add children");
+        children.push(Child(_firstName, _lastName, _age));
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
+    //Creation d'un mariage (a faire avec en object (enfant, mari, marie, date))
+    //TODO
+    
+    //On retourn l'argent utilisé pour le mariage
     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
-    }
-
-    function transferBis(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-
-        // Notify off-chain applications of the transfer.
-        emit Transfer(msg.sender, to, amount);
     }
 }
